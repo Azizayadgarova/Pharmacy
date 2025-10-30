35d3e32e572c6c4c69414d56ba3eae0dbdf716a6
yadgarovtthjjiiiiiiiiiiiiiiiiiiii <mamuray819@gmail.com>
2025-10-30 13:35:58 +0500

    done

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerAdmin, loginAdmin, updateAdmin } from "../controllers/auth.controller.js";
+import { registerAdmin, loginAdmin } from "../controllers/auth.controller.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
@@ -9,8 +9,5 @@ router.post("/register", registerAdmin);
 
 // Login uchun
 router.post("/login", loginAdmin);
-
-// Admin ma'lumotlarini yangilash
-router.put("/update", protect, updateAdmin);
 
 export default router;
