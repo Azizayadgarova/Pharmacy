aadc5c816392240b3f28804ff50ce519e688a82a
yadgarovtthjjiiiiiiiiiiiiiiiiiiii <mamuray819@gmail.com>
2025-12-12 22:18:12 +0500

    done

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,14 @@ import medicinesRouter from "./medicines.js";
 import authRouter from "./auth.js";
 import ordersRouter from "./orders.js";
 import advertisementsRouter from "./advertisements.js";
+import utilsRoutes from "./utilsRoutes.js"; // ← to'g'ri nom bilan import
 
 const r = Router();
+
 r.use("/auth", authRouter);
 r.use("/medicines", medicinesRouter);
 r.use("/orders", ordersRouter);
 r.use("/ads", advertisementsRouter);
+r.use("/utils", utilsRoutes); // ← bu endi ishlaydi
 
 export default r;
